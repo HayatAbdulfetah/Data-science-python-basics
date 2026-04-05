25447628ad502744d27c13b04fb484fc19a9298c
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-04-06 01:05:22 +0300

    function to find the largest number in a list

---
+++ level2.py
@@ -0,0 +1,7 @@
+# Find the largest number in a list
+# if there is a list of unsorted numbers
+# just use max() function
+
+nums = [2,3,6,7,9,11,4]
+maximum_num = max(nums)
+print(maximum_num) # it will print 11 which is the largest number from the list
